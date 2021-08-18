5cf2ab972acc8400c8f0b94e275f7079a9e90e5d
magnitopic <magnitopic@gmail.com>
2021-08-19 01:07:41 +0200

    Basic programs for joystick

---
+++ joystick/basic.py
@@ -0,0 +1,11 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+while True:
+	event = sense.stick.wait_for_event()
+	print("The joystick was {} {}".format(event.action, event.direction))
+	sleep(0.3)
+	event = sense.stick.wait_for_event()
+	print("The joystick was {} {}".format(event.action, event.direction))
