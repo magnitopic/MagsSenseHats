4ff913e2fcb645a689025420889e9677eed010bb
magnitopic <magnitopic@gmail.com>
2022-05-05 19:49:39 +0200

    Files and data from json

--- DojoProyect/server/functions.py
+++ DojoProyect/server/functions.py
@@ -1,5 +1,19 @@
-from pyparsing import opAssoc
+import json
+import os
 
+def getInfo(name):
+    if name not in getNames():
+        return None
+    with open(f'../data/{name}.json') as f:
+        file = json.load(f)
+    return file
 
-def getInfo():
-    return "YESS"
+def getNames():
+	names=[]
+	for file in os.listdir("../data"):
+		if file.endswith(".json"):
+			names.append(os.path.splitext(file)[0])
+	return names
+
+if __name__=="__main__":
+	pass
