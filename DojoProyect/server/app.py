c94059379be53aa76287fa1946db6e84629239ad
magnitopic <magnitopic@gmail.com>
2022-05-06 18:57:34 +0200

    New info file

--- DojoProyect/server/app.py
+++ DojoProyect/server/app.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, render_template, jsonify
 from flask.wrappers import Response
 import os
-from functions import getInfo
+from functions import getInfo,getNames
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def main():
-    pyload = getInfo()
-    return render_template("index.html", pyload=pyload)
+    return render_template("index.html", names=getNames())
 
 
 if __name__ == "__main__":
